6bfbc8b4e5b3bc7628e7687068dbae4087b13e15
pragneshok <pragneshrupapara@gmail.com>
2014-07-01 15:16:42 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,8 +5,11 @@ $sandbox = TRUE;
 // Set PayPal API version and credentials.
 $api_version = '93.0';
 $api_endpoint = $sandbox ? 'https://api-3t.sandbox.paypal.com/nvp' : 'https://api-3t.paypal.com/nvp';
-$api_username = $sandbox ? 'SANDBOX_USERNAME_GOES_HERE' : 'LIVE_USERNAME_GOES_HERE';
+/*$api_username = $sandbox ? 'SANDBOX_USERNAME_GOES_HERE' : 'LIVE_USERNAME_GOES_HERE';
 $api_password = $sandbox ? 'SANDBOX_PASSWORD_GOES_HERE' : 'LIVE_PASSWORD_GOES_HERE';
-$api_signature = $sandbox ? 'SANDBOX_SIGNATURE_GOES_HERE' : 'LIVE_SIGNATURE_GOES_HERE';
+$api_signature = $sandbox ? 'SANDBOX_SIGNATURE_GOES_HERE' : 'LIVE_SIGNATURE_GOES_HERE';*/
 
+$api_username 		= 'Keyur.Joshi-facilitator_api1.imobdevtech.com'; //PayPal API Username
+$api_password 		= '1370586731'; //Paypal API password
+$api_signature 		= 'An5ns1Kso7MWUdW4ErQKJJJ4qi4-AXQejVf2IZobXvy9KwoTwVAMtxFn'; //Paypal API Signature
 
